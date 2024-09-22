d2181c000069fc41ad03bbb172705ffba1ee4acc
astro_dally <animemedia64@gmail.com>
2024-09-22 20:12:12 +0530

    updated

--- frontend/src/pages/PlaceOrder/QrConfirm.jsx
+++ frontend/src/pages/PlaceOrder/QrConfirm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Qrconfirm.css";
+import "../PlaceOrder/QrConfirm.css";
 import qrImage from "../../assets/PaymentQR.png";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
